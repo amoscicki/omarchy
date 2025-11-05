ab72dd31ed20a26fd38f7ba5c8fb535f25c553ff
amoscicki <82836981+amoscicki@users.noreply.github.com>
2025-11-05 01:09:02 +0000

    Allow ext4 installs by disabling Btrfs-only steps

--- install/login/all.sh
+++ install/login/all.sh
@@ -1,4 +1,8 @@
 run_logged $OMARCHY_INSTALL/login/plymouth.sh
 run_logged $OMARCHY_INSTALL/login/default-keyring.sh
 run_logged $OMARCHY_INSTALL/login/sddm.sh
-run_logged $OMARCHY_INSTALL/login/limine-snapper.sh
+if [[ -n ${OMARCHY_DISABLE_BTRFS_FEATURES:-} ]]; then
+  echo "Skipping Limine + Snapper setup (Btrfs root filesystem not detected)"
+else
+  run_logged $OMARCHY_INSTALL/login/limine-snapper.sh
+fi
